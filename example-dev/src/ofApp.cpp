a967c6cdcfa5c8e00532d4aa749df7cb84d991d3
jason van cleave <jvcleave@gmail.com>
2015-11-17 23:13:45 +0000

    remove sub

---
+++ example-dev/src/ofApp.cpp
@@ -0,0 +1,31 @@
+#include "ofApp.h"
+
+
+
+
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+
+    string videoPath = ofToDataPath("Timecoded_Big_bunny_1.mov", true);
+    ofLogVerbose() << "videoPath: " << videoPath;
+    videoFile.setup(videoPath);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
